5c44e6f14619bbdf340a35397827681211362b0e
Your Name <almashossain121@gmail.com>
2021-01-17 14:37:34 -0800

    commit

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname + '/dist/front-end'))
+app.use(express.static(__dirname + '/dist/frontEnd'))
 app.listen(process.env.PORT || 8000);
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/front-end/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/frontEnd/index.html'))
 })
 
 console.log('Console listening');
